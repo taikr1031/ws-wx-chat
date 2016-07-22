4d8f6a704d20f4cc118c57e07ecc53d9c68e20b4
taikr1031 <taikongren1@163.com>
2016-07-23 00:07:18 +0800

    second commit

--- src/main/java/com/zm/controller/LoginController.java
+++ src/main/java/com/zm/controller/LoginController.java
@@ -15,36 +15,12 @@ import java.util.Date;
 
 @Controller
 public class LoginController {
-//  @Autowired
-//  @Resource
-//  private SimpMessagingTemplate messagingTemplate;
+
   @Autowired
   private ParticipantRepository participantRepository;
 
   private static final String LOGIN = "/app/chat.login";
   private static final String LOGOUT = "/app/chat.logout";
-
-//  public LoginController() {
-//  }
-//  @Autowired
-//  public LoginController(SimpMessagingTemplate messagingTemplate) {
-//	this.messagingTemplate = messagingTemplate;
-//  }
-
-  @RequestMapping(value = "login", method = RequestMethod.POST)
-  public String login(HttpServletRequest httpRequest, User user) throws ServletException {
-	user.setTime(new Date());
-	httpRequest.getSession().setAttribute(Constants.SESSION_USERNAME, user);
-//	SystemWebSocketHandler handler = new SystemWebSocketHandler();
-//	handler.sendMessageToUser(user.getUsername(), new TextMessage(user.getUsername() + "load"));
-
-//	messagingTemplate.convertAndSend(LOGIN, user);
-//	if (participantRepository.getActiveSessions().containsKey(httpRequest.getSession().getId())) {
-//	  messagingTemplate.convertAndSend(LOGOUT, participantRepository.getActiveSessions().get(httpRequest.getSession().getId()));
-//	}
-	participantRepository.add(httpRequest.getSession().getId(), user);
-	return "redirect:/chart";
-  }
 
   @RequestMapping("/loginSession/{openid}")
   public void loginSession(@PathVariable("openid") String openid, HttpServletRequest httpRequest) throws ServletException {
